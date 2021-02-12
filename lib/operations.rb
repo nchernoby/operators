098a94b431cd429c024664a91ba4c09d9b683791
Nicole Chernoby <n.chernoby@gmail.com>
2021-02-11 22:22:25 -0800

    commit tree cf6214a17f85c75c544d994b0b11ccee0bc4697b

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+    if ((speed < 40) || (speed > 60))
+        true
+    else ((speed < 60) || (speed > 40))
+        false
+    end
 end
 
 
-
 def not_safe?(speed)
-	
+	(speed < 40) || (speed > 60) ? true : false
 end
 	
 
